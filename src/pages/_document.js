0da38dee91a2cc9eb32c8eca1ce8f03290d1be43
alim-2023 <alim@crystallitedigital.com>
2023-11-06 22:11:01 +0500

    add meta

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,6 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
+      <Head>
+      <meta name="google-site-verification" content="LeCTlf6XFkdNEsNBD622lw9OyimFlgL8Vbkq6ZiYYxM" />
+      </Head>
       <Head />
       <body>
         <Main />
