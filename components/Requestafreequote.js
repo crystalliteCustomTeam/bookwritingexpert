220f8b4af0f229a3966e5906acf8e813ef54780f
alim-2023 <alim@crystallitedigital.com>
2023-11-16 18:11:30 +0500

    requestafreequte form

--- components/Requestafreequote.js
+++ components/Requestafreequote.js
@@ -7,11 +7,22 @@ import { useState } from "react";
 import { BsFillTelephoneFill } from 'react-icons/bs';
 import { MdEmail } from 'react-icons/md';
 import Router from 'next/router';
-import axios from "axios";
+import Axios from "axios";
+import { useEffect } from 'react';
 import { useRouter } from 'next/router';
 
 
 const Requestafreequote = () => {
+
+  const [ip, setIP] = useState('');
+  //creating function to load ip address from the API
+  const getIPData = async () => {
+    const res = await Axios.get('https://geolocation-db.com/json/f2e84010-e1e9-11ed-b2f8-6b70106be3c8');
+    setIP(res.data);
+  }
+  useEffect(() => {
+    getIPData()
+  }, [])
 
 
   const [score, setScore] = useState('Submit');
@@ -53,6 +64,30 @@ const Requestafreequote = () => {
         console.log(`Response Successed ${res}`)
       }
     })
+
+    var currentdate = new Date().toLocaleString() + ''
+    let headersList = {
+        "Accept": "*/*",
+        "User-Agent": "Thunder Client (https://www.thunderclient.com)",
+        "Authorization": "Bearer ke2br2ubssi4l8mxswjjxohtd37nzexy042l2eer",
+        "Content-Type": "application/json"
+       }
+       
+       let bodyContent = JSON.stringify({
+        "IP": `${ip.IPv4} - ${ip.country_name} - ${ip.city}`,
+        "Brand": "BOOK-WRITING-EXPERT",
+        "Page": `${currentRoute}`,
+        "Date": currentdate,
+        "Time": currentdate,
+        "JSON": JSONdata,
+       
+      });
+       
+     await fetch("https://sheetdb.io/api/v1/1ownp6p7a9xpi", { 
+         method: "POST",
+         body: bodyContent,
+         headers: headersList
+       });
 
     const { pathname } = Router
     if (pathname == pathname) {
