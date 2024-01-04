1bea2a2ea317ad26f1f3fcad62d9610ef6233bd7
alim-2023 <alim@crystallitedigital.com>
2024-01-04 21:21:59 +0500

    outsidev.6

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -58,23 +58,7 @@ export default function App({ Component, pageProps }) {
       </Head>
 
   
-      <Script strategy="lazyOnload" src="https://www.googletagmanager.com/gtag/js?id=G-HE06TX00YE"></Script>
-          <Script strategy="lazyOnload">
-            {` window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'G-HE06TX00YE');
-        `}
-          </Script>
-          {/* Other meta tags and head elements */}
-          <Script strategy="lazyOnload">
-            {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-K994MT85');
-        `}
-          </Script>
+
 
 
 
@@ -92,7 +76,23 @@ export default function App({ Component, pageProps }) {
 
       {imagesLoaded ?
         <>
-         
+          <Script strategy="lazyOnload" src="https://www.googletagmanager.com/gtag/js?id=G-HE06TX00YE"></Script>
+          <Script strategy="lazyOnload">
+            {` window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-HE06TX00YE');
+        `}
+          </Script>
+          {/* Other meta tags and head elements */}
+          <Script strategy="lazyOnload">
+            {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-K994MT85');
+        `}
+          </Script>
           < Header />
           <Pixel />
           
