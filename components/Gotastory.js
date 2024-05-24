e8a55298dc658bca56d8dd19002337c68e284d9e
devsahal-crystallite <143542001+devsahal-crystallite@users.noreply.github.com>
2024-05-25 02:34:18 +0500

    video add

--- components/Gotastory.js
+++ components/Gotastory.js
@@ -10,7 +10,7 @@ const Gotastory = (props) => {
         <Container fluid>
           <Row>
             <Col md={12} className='p-0'>
-              <video className={`${styles.bgVideo}`} width={890} autoPlay muted loop src="https://player.vimeo.com/progressive_redirect/playback/950057618/rendition/1080p/file.mp4?loc=external&log_user=0&signature=2f66c3a010fb9e04ad78b5c128befb1500e0bd02d8fd397b7c074615bd7e4f21" type="video/mp4"></video>
+              <video className={`${styles.bgVideo}`} width={890} autoPlay muted loop src="https://player.vimeo.com/progressive_redirect/playback/950057618/rendition/1080p/file.mp4?loc=external&log_user=0&signature=2f66c3a010fb9e04ad78b5c128befb1500e0bd02d8fd397b7c074615bd7e4f21" type="video/mp4"></video> 
             </Col>
           </Row>
         </Container>
