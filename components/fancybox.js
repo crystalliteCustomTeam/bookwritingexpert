b80239f444d5cd225a2e15fb768b84009745ec81
Murtaza Zaidi <murtaza.zaidi@crystallite.com>
2024-08-21 04:38:31 +0500

    video reviews added

---
+++ components/fancybox.js
@@ -0,0 +1,21 @@
+import React, { useEffect } from "react";
+
+import { Fancybox as NativeFancybox } from "@fancyapps/ui";
+import "@fancyapps/ui/dist/fancybox/fancybox.css";
+function Fancybox(props) {
+  const delegate = props.delegate || "[data-fancybox]";
+
+  useEffect(() => {
+    const opts = props.options || {};
+
+    NativeFancybox.bind(delegate, opts);
+
+    return () => {
+      NativeFancybox.destroy();
+    };
+  }, []);
+
+  return <>{props.children}</>;
+}
+
+export default Fancybox;
