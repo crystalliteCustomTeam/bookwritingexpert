8047c56c0b4de8712f1f5aa2cb3858f91c70b005
Murtaza Zaidi <murtaza.zaidi@crystallite.com>
2024-09-18 22:33:38 +0500

    import dynamic

--- src/pages/book-marketing-services.js
+++ src/pages/book-marketing-services.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Head from 'next/head'
 import Link from 'next/link'
+import dynamic from 'next/dynamic'
 import { Container, Row, Col } from 'react-bootstrap'
 
 // images
