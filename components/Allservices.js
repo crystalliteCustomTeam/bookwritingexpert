2e371b783a709f02fa23f9295cd9a0143594b2d5
dev-mr <m.rizwan@crystal-lite.net>
2024-09-19 06:06:25 +0500

    Fix Css

--- components/Allservices.js
+++ components/Allservices.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import styles from '@/styles/Allservices.module.min.css'
+import styles from '@/styles/Allservices.module.css'
 import Image from 'next/image'
 import { Container, Row, Col } from 'react-bootstrap'
 
