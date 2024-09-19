fd1f4a5bd149f714440e63c17a4ee41fe97ba077
dev-mr <m.rizwan@crystal-lite.net>
2024-09-19 05:29:32 +0500

    Fix Third Party Codes

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,7 @@
 import "@/styles/globals.css";
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
-const ZENDESK_KEY = "d1d4aa8a-5573-473a-b921-5b6a8eb467ad";
+// const ZENDESK_KEY = "d1d4aa8a-5573-473a-b921-5b6a8eb467ad";
 import Zendesk, { ZendeskAPI } from "../pages/ZendexConfig";
 import Script from "next/script";
 import Pixel from "../../components/Pixel";
@@ -37,9 +37,6 @@ const primaryFont = localFont({
 
 export default function App({ Component, pageProps }) {
   const router = useRouter();
-  const handleLoaded = () => {
-    zE("webWidget:on", "open", function () { });
-  };
   const sluginer = router.asPath;
   const weblink = "https://www.bookwritingexperts.com";
   const newlop = router.asPath == "/bookexperlp";
@@ -53,16 +50,31 @@ export default function App({ Component, pageProps }) {
         <link rel="canonical" href={newcol} />
       </Head>
       <Metas />
-      <Script src="https://www.googletagmanager.com/gtag/js?id=AW-411006583"></Script>
-      <GoogleAnalytics gaId="AW-411006583" />
-      <GoogleTagManager gtmId="GTM-K994MT85" />
       <div className={`${primaryFont.variable} ${fontSecondary.variable}`}>
         {newlop || newbooklp ? <Headerlp /> : <Header />}
-        <Pixel />
         <Component {...pageProps} />
         {newlop ? <Footerlp /> : (newbooklp ? <NewbooklpFooter /> : <Footer />)}
       </div>
-      <Zendesk defer zendeskKey={ZENDESK_KEY} onLoaded={handleLoaded} />
+      <Script id="facebook-snippet">
+        {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '8267519626599421');
+            fbq('track', 'PageView');
+        `}
+      </Script>
+      <GoogleAnalytics gaId="AW-411006583" />
+      <GoogleTagManager gtmId="GTM-K994MT85" />
+      <Script
+        id="ze-snippet"
+        src="https://static.zdassets.com/ekr/snippet.js?key=d1d4aa8a-5573-473a-b921-5b6a8eb467ad"
+      />
     </>
   );
 }
