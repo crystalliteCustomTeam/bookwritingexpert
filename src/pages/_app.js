e67374df8a9ad0af22bda65bd0bf304693061bd8
Harry Kennedy <72253270+Harrythedevelopercs@users.noreply.github.com>
2025-01-21 02:02:29 +0500

    new chat

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -57,16 +57,16 @@ export default function App({ Component, pageProps }) {
         <Component {...pageProps} />
         {newlop ? <Footerlp /> : newbooklp ? <NewbooklpFooter /> : <Footer />}
       </div>
-      <Script>
+      <Script id="tawkto-snippet">
         {`
-        var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
-        (function() {
-          var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
-          s1.async = true;
-          s1.src = 'https://embed.tawk.to/677eaf1949e2fd8dfe046557/1ih3dnvtn';
-          s1.charset = 'UTF-8';
-          s1.setAttribute('crossorigin', '*');
-          s0.parentNode.insertBefore(s1, s0);
+          var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+          (function(){
+          var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+          s1.async=true;
+          s1.src='https://embed.tawk.to/678eb3553a8427326071da6b/1ii2mp2bs';
+          s1.charset='UTF-8';
+          s1.setAttribute('crossorigin','*');
+          s0.parentNode.insertBefore(s1,s0);
         })();
         `}
       </Script>
